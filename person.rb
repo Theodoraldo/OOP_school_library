f4dc23ea7536c688f4d8a29de52eeb68ee36e926
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-24 18:14:10 +0000

    Remove linters errors

--- person.rb
+++ person.rb
@@ -1,20 +1,20 @@
 class Person
-  attr_accessor(:id, :name, :age, :parent_permission)
+  attr_accessor :id, :name, :age, :parent_permission
 
-  def initialize (age, parent_permission: true, name = "Unknown")
-    @id =  Time.now.strftime("%Y%d%m%H%M%S%Z").to_i
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = Time.now.strftime('%Y%d%m%H%M%S%Z').to_i
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    of_age? || @parent_permission ? 'true' : 'false'
+    of_age? || @parent_permission
   end
 
   private
 
   def of_age?
-    @age >= 18 ? true : false  
+    @age >= 18
   end
 end
