81fa9d5d4c0037955e9d5f02a2ad114ceece45f8
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-24 17:23:02 +0000

    Correct linters errors

--- person.rb
+++ person.rb
@@ -1,8 +1,7 @@
 class Person
-
   attr_accessor(:id, :name, :age, :parent_permission)
 
-  def initialize(name = 'Unknown', age, parent_permission: true)
+  def initialize (age, parent_permission: true, name = "Unknown")
     @id =  Time.now.strftime("%Y%d%m%H%M%S%Z").to_i
     @name = name
     @age = age
@@ -10,7 +9,7 @@ class Person
   end
 
   def can_use_services?
-    of_age? || @parent_permission ? true : false
+    of_age? || @parent_permission ? 'true' : 'false'
   end
 
   private
