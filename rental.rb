a20acbf8d15f23dfda185d263d75528aa364fd0c
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-29 12:18:31 +0000

    Modify rental.rb file for performance

--- rental.rb
+++ rental.rb
@@ -5,7 +5,7 @@ class Rental
     @date = date
     @person = person
     @book = book
-    person.add_rental(self)
-    book.add_rental(self)
+    person.rentals << self
+    book.rentals << self
   end
 end
